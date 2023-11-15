c9caacf8ef67faeb87a8b22d1190f113c73d3605
NoorHaan25 <nourhantaha588@gmail.com>
2023-11-16 01:07:34 +0200

    first project with SASS
    add many of sections :
    header , top-home , features , brands and footer

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+// let request = new XMLHttpRequest()
+// request.open("GET", "https://jsonplaceholder.typicode.com/posts")
+// request.responseType ="json"
+// request.send()
+// request.onload = function(){
+//   console.log(request.response);
+// }
