d51f841a099bb3d4b966b50d212ff1dcdce2fdbb
NoorHaan25 <nourhantaha588@gmail.com>
2023-12-26 02:49:16 +0200

    add function (sliderImages())

--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,73 @@
-import { openedNavbar , closedNavbar } from "./navbar.js";
+import { openedNavbar, closedNavbar } from "./navbar.js";
 import { dropdDownMenu } from "./dropdown.js";
 
 /*                                             start section countdown                                                */
-let countDown =new Date("jan,1 2024 24:00:00").getTime();
-let count=setInterval(() => {
-    let now=new Date().getTime();
-    let differenceDate=countDown-now;
-    let days=Math.floor(differenceDate/(24*60*60*1000));
-    let hours=Math.floor(differenceDate % (24*60*60*1000) / (60*60*1000));
-    let minutes=Math.floor(differenceDate % (60*60*1000) / (60*1000));
-    let seconds=Math.floor(differenceDate % (60*1000) / (1000));
-    document.getElementById("counter-days").innerHTML=days;
-    document.getElementById("counter-hours").innerHTML=hours;
-    document.getElementById("counter-minutes").innerHTML=minutes;
-    document.getElementById("counter-seconds").innerHTML=seconds;
-    
-if(differenceDate < 0){
-    clearInterval(count);
-}
+let countDown = new Date("jan,1 2024 24:00:00").getTime();
+let count = setInterval(() => {
+    let now = new Date().getTime();
+    let differenceDate = countDown - now;
+    let days = Math.floor(differenceDate / (24 * 60 * 60 * 1000));
+    let hours = Math.floor(
+        (differenceDate % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+    );
+    let minutes = Math.floor((differenceDate % (60 * 60 * 1000)) / (60 * 1000));
+    let seconds = Math.floor((differenceDate % (60 * 1000)) / 1000);
+    document.getElementById("counter-days").innerHTML = days;
+    document.getElementById("counter-hours").innerHTML = hours;
+    document.getElementById("counter-minutes").innerHTML = minutes;
+    document.getElementById("counter-seconds").innerHTML = seconds;
+
+    if (differenceDate < 0) {
+        clearInterval(count);
+    }
 }, 1000);
 /*                                             end section countdown                                                */
 const userName = localStorage.getItem("userName");
 const notExistingAccount = document.getElementById("not-existing-account");
 const existingAccount = document.getElementById("existing-account");
-console.log('userName', userName);
-console.log('index: ' , notExistingAccount , existingAccount);
-if(userName !== null ){
-    console.log('index: ' , userName);
-    notExistingAccount.style.cssText= "display:none;"
-    existingAccount.style.cssText="display:block;"
+console.log("userName", userName);
+console.log("index: ", notExistingAccount, existingAccount);
+if (userName !== null) {
+    console.log("index: ", userName);
+    notExistingAccount.style.cssText = "display:none;";
+    existingAccount.style.cssText = "display:block;";
+} else if (userName === null) {
+    existingAccount.style.cssText = "display:none;";
+    notExistingAccount.style.cssText = "display:block;";
 }
-else if(userName === null){
-    existingAccount.style.cssText="display:none;"
-    notExistingAccount.style.cssText= "display:block;"
+function sliderImages(){
+    const images = document.querySelectorAll(".img-slider");
+    const activeImage = document.getElementsByClassName("active-img");
+    const feature = document.getElementById("feature");
+    const slideUp = document.getElementById("slide-up");
+    const slideDown = document.getElementById("slide-down");
+    const navSlider = document.getElementById("nav-slider");
+    // console.log('slideUp: ', slideUp , 'slideDown: ', slideDown);
+    // console.log("feature", feature);
+    // console.log("images", activeImage);
+    images.forEach((image) => {
+        // console.log('image', image);
+        image.addEventListener("click", function () {
+            console.log("image", activeImage);
+            if (activeImage.length > 0) {
+                //console.log('activeImage[0]' , activeImage);
+                activeImage[0].classList.remove("active-img");
+            }
+            this.classList.add("active-img");
+            feature.src = this.src;
+        });
+        
+    });
+    slideUp.addEventListener("click", function(){
+        // console.log(this);
+        navSlider.scrollTop += 180
+    })
+    slideDown.addEventListener("click", function(){
+        // console.log(this);
+        navSlider.scrollTop -= 180
+    })
 }
+sliderImages();
 openedNavbar();
 closedNavbar();
 dropdDownMenu();
